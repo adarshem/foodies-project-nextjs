401c9983398fb94e043089502492f37e39ea8eb6
adarshem <adarshem@gmail.com>
2024-12-22 10:38:21 -0800

    Migrated the nextjs JS app to TS

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home(): React.JSX.Element {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+    </main>
+  );
+}
